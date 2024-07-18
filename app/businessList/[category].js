0a14a0a28ada5bf1d87242a85733f86a989958a8
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-07-18 15:44:46 +0530

    Add the activity indicator

--- app/businessList/[category].js
+++ app/businessList/[category].js
@@ -1,55 +1,68 @@
-import { View, Text, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from '../../configs/FirbaseConfig';
-import BusinessListCard from '../../components/BusinessList/BusinessListCard';
-import { Colors } from '../../constants/Colors';
+import { db } from "../../configs/FirbaseConfig";
+import BusinessListCard from "../../components/BusinessList/BusinessListCard";
+import { Colors } from "../../constants/Colors";
 
 export default function BusinessListByCategory() {
+  const navigation = useNavigation();
+  const { category } = useLocalSearchParams();
+  const [businessList, setBusinessList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-   const navigation = useNavigation();
-   const { category } = useLocalSearchParams();
-   const [businessList, setBusinessList] = useState([]);
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTitle: category,
+    });
+    getBusinessList();
+  }, []);
 
-   useEffect(() => {
-      navigation.setOptions({
-         headerShown: true,
-         headerTitle: category
-      })
-      getBusinessList();
-   }, [])
-
-   /* used to get business list by category */
-   const getBusinessList = async () => {
-      setBusinessList([]);
-      const q = query(collection(db, "BusinessList"), where("category", "==", category));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-           setBusinessList((prev) => [...prev, doc.data()]);
-         });
-   }
+  /* used to get business list by category */
+  const getBusinessList = async () => {
+    setLoading(true);
+    setBusinessList([]);
+    const q = query(
+      collection(db, "BusinessList"),
+      where("category", "==", category)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setBusinessList((prev) => [...prev, doc.data()]);
+    });
+    setLoading(false);
+  };
 
   return (
     <View>
-      {
-         businessList.length > 0 ? (
-            <FlatList 
-               data={businessList}
-               renderItem={({ item, index }) => (
-                  <BusinessListCard key={index} business={item} />
-               )}
-            />
-         ) : (
-            <Text style={{
-               textAlign: 'center',
-               marginTop: "50%",
-               fontFamily: 'outfit-bold',
-               fontSize: 20,
-               color: Colors.GRAY
-            }}>No business found</Text>
-         )
-      }
+      {businessList.length > 0 && loading == false ? (
+        <FlatList
+          data={businessList}
+          onRefresh={getBusinessList}
+          refreshing={loading}
+          renderItem={({ item, index }) => (
+            <BusinessListCard key={index} business={item} />
+          )}
+        />
+      ) : loading ? (
+        <ActivityIndicator size={"large"} color={Colors.PRIMARY} style={{
+            marginTop: "60%",
+        }} />
+      ) : (
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: "50%",
+            fontFamily: "outfit-bold",
+            fontSize: 20,
+            color: Colors.GRAY,
+          }}
+        >
+          No business found
+        </Text>
+      )}
     </View>
-  )
+  );
 }
